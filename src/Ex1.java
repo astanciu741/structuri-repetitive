54c547da77bf5568f88dd1b4f770215b831fbcb7
andreea <actsanciu741@gmail.com>
2019-04-23 23:06:04 +0100

    Initial commit

---
+++ src/Ex1.java
@@ -0,0 +1,23 @@
+
+public class Ex1 {
+    public Object ex1;
+
+    public static void afisare() {
+        String a = "####";
+            int i;
+        for (i = 1; i <= 4; i++) {
+            System.out.print(a.substring(0,i)+" ");
+        }}
+    public static void afisare2() {
+        String b = "####";
+        int j;
+        for (j = 3; j >= 1; j--) {
+            System.out.print(b.substring(0,j)+" ");
+            //System.out.println("salut");
+        }
+
+    }
+
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//package ex1;
+
+public class Main {
+    public static void main(String[] args) {
+    Ex1 ex1= new Ex1();
+        ex1.afisare();
+        ex1.afisare2();
+
+    }
+}
