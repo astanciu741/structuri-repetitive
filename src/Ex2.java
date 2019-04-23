1ac5eeee6ded566bdf94f29524d513092f6755ee
andreea <actsanciu741@gmail.com>
2019-04-23 23:15:54 +0100

    2

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Ex2 {
+
+    public static void transformare(int n){
+        if (n%2==0){n=n/2;}
+        else {n=3*n+1;}
+        System.out.println("\n");
+    System.out.println(n);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@
 
 public class Main {
     public static void main(String[] args) {
-    Ex1 ex1= new Ex1();
+        System.out.println("Rezolvare exercitiu 1");
+        Ex1 ex1= new Ex1();
         ex1.afisare();
         ex1.afisare2();
+        System.out.println("\n");
+        System.out.println("Rezolvare exercitiu 2");
+        Ex2 ex2= new Ex2();
+        ex2.transformare(31);
+        ex2.transformare(66);
+
 
     }
 }
